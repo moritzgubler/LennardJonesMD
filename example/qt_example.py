f55fb318f0a68bff80e163deb438f345f9a5c73a
Moritz Gubler <moritz.gubler@gmail.com>
2023-01-22 20:28:41 +0100

    qt working

--- example/qt_example.py
+++ example/qt_example.py
@@ -3,11 +3,23 @@ import time
 from pyqtgraph.Qt import QtCore, QtGui, QtWidgets
 import numpy as np
 import pyqtgraph as pg
-
+import shelve
+import time
 
 class App(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(App, self).__init__(parent)
+
+
+        pg.setConfigOption('background', 'w')
+        pg.setConfigOption('foreground', 'k')
+
+        self.pos_shelve = shelve.open('fp.npz')
+        self.col_shelve = shelve.open('fc.npz')
+
+        self.targetfps = 60
+        self.targetTime = 1 / self.targetfps
+        self.ncolors = 8
 
         #### Create Gui Elements ###########
         self.mainbox = QtWidgets.QWidget()
@@ -22,42 +34,69 @@ class App(QtWidgets.QMainWindow):
 
         self.view = self.canvas.addViewBox()
         self.view.setAspectLocked(True)
-        self.view.setRange(QtCore.QRectF(0,0, 100, 100))
+        self.p0 = self.pos_shelve[str(0)]
+        self.c0 = self.col_shelve[str(0)]
 
-        #  image plot
-        self.img = pg.ImageItem(border='w')
-        self.view.addItem(self.img)
+        self.offset=2.0
+        self.minx = np.min(self.p0[:,0]) - self.offset
+        self.maxx = np.max(self.p0[:,0]) + self.offset
+        self.miny = np.min(self.p0[:,1]) - self.offset
+        self.maxy = np.max(self.p0[:,1]) + self.offset
+        self.dx = self.maxx - self.minx
+        self.dy = self.maxy - self.miny
+
+        self.brushList = []
+        for i in range(self.ncolors):
+            self.brushList.append(QtGui.QBrush( QtGui.QColor(int(i*256/self.ncolors), 0, 0, 255)))
+
+        self.brushes = []
+        for i, c in enumerate(self.c0):
+            self.brushes.append(self.brushList[int(c[0]*(self.ncolors-1))])
+
+        
+
+
+        self.view.setRange(QtCore.QRectF(self.minx,self.miny, self.dx, self.dy))
+
+        self.sc = pg.ScatterPlotItem()
+        self.view.addItem(self.sc)
 
         self.canvas.nextRow()
-        #  line plot
-        self.otherplot = self.canvas.addPlot()
-        self.h2 = self.otherplot.plot(pen='y')
 
 
         #### Set Data  #####################
-
-        self.x = np.linspace(0,50., num=100)
-        self.X,self.Y = np.meshgrid(self.x,self.x)
 
         self.counter = 0
         self.fps = 0.
         self.lastupdate = time.time()
 
-        #### Start  #####################
+
+        self.n_steps = len(self.pos_shelve)
+        # self.p = self.pos_shelve[str(self.counter)]
         self._update()
 
     def _update(self):
 
-        self.data = np.sin(self.X/3.+self.counter/9.)*np.cos(self.Y/3.+self.counter/9.)
-        self.ydata = np.sin(self.x/3.+ self.counter/9.)
+        self.p = self.pos_shelve[str(self.counter)]
+        self.col = self.col_shelve[str(self.counter)]
 
-        self.img.setImage(self.data)
-        self.h2.setData(self.ydata)
+        for i, c in enumerate(self.col):
+            self.brushes[i] = self.brushList[int(c[0]*(self.ncolors-1))]
+
+        # t1 = time.time()
+        self.sc.setData(self.p[:, 0], self.p[:, 1], pen=None, brush=self.brushes)
+        # t2 = time.time()
+        # print((t2 - t1))
 
         now = time.time()
         dt = (now-self.lastupdate)
         if dt <= 0:
             dt = 0.000000000001
+        if dt <= self.targetTime:
+            print("delaying")
+            time.sleep(self.targetTime - dt)
+        now = time.time()
+        dt = (now-self.lastupdate)
         fps2 = 1.0 / dt
         self.lastupdate = now
         self.fps = self.fps * 0.9 + fps2 * 0.1
@@ -65,6 +104,8 @@ class App(QtWidgets.QMainWindow):
         self.label.setText(tx)
         QtCore.QTimer.singleShot(1, self._update)
         self.counter += 1
+        if self.counter >= self.n_steps:
+            self.counter = 0
 
 
 if __name__ == '__main__':
